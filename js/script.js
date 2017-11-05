b9a30e3125412b7df33cc22f7735b01f51cb476f
Kiearn McCormick <octavian13@shaw.ca>
2017-11-05 14:08:41 -0800

    UI value initialization progress

--- js/script.js
+++ js/script.js
@@ -1,9 +1,54 @@
 var DATAURL = "https://beagle-boop.firebaseio.com/boards/938172/data.json"
 var BOARDURL = "https://beagle-boop.firebaseio.com/boards.json"
+var boardData;
 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
+
+$(document).ready(function () {
+
+
+    $.get({
+        url: BOARDURL
+    })
+        .done(function (data) {
+            boardData = data;
+            console.log("Data: ", boardData);
+
+            var js = 1
+            for (board in boardData) {
+
+                console.log(boardData[board])
+                var item = $('#hiddenLink').clone();
+                item.attr("id", "boardItem")
+                item.attr("onclick", 'selectBoard("' + board + '")');
+                item.text(capitalizeFirstLetter(board));
+                $('#myDropdown').append(item);
+
+            }
+
+            console.log("Done.");
+        })
+
+    $('#btn').click(function () {
+        var id = $(this).attr('id');
+        alert("test");
+    });
+
+    /*     $.get({
+            url: DATAURL
+        })
+            .done(function (data) {
+                console.log("Data: ", data);
+                console.log("Current time ", Date.now());
+                console.log("Done.");
+            }) */
+});
+
+//TODO 
+// function updateData() {
+//         //TODO: get data from firebase and update UI
+//     });
+// }
+// timeInterval = setInterval(updateData, 1000);
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function (event) {
@@ -20,49 +65,16 @@ window.onclick = function (event) {
     }
 }
 
-function selectBoard(data) {
-    console.log("test: " + data);
+function selectBoard(boardName) {
+    console.log("selectBoard: " + boardName);
+    console.log(boardData[boardName].ownerName);
+    $("#currentBoard").text(boardData[boardName].ownerName);
+    $("#timeInput").val(boardData[boardName].timeToAnswer);
+    $("#lives").val(boardData[boardName].lives);
+
+    $("#boardForm").show();
 }
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
-
-$(document).ready(function () {
-
-
-    $.get({
-        url: BOARDURL
-    })
-        .done(function (data) {
-            console.log("Data: ", data);
-
-            var js = 1
-            for (i in data) {
-
-                console.log(data[i])
-                var item = $('#hiddenLink').clone();
-                item.attr("id", "boardItem")
-                item.attr("onclick", 'selectBoard("' + i + '")');
-                item.text(capitalizeFirstLetter(i));
-                $('#myDropdown').append(item);
-
-            }
-
-            console.log("Done.");
-        })
-
-    $('#btn').click(function () {
-        var id = $(this).attr('id');
-        alert(id);
-    });
-
-    /*     $.get({
-            url: DATAURL
-        })
-            .done(function (data) {
-                console.log("Data: ", data);
-                console.log("Current time ", Date.now());
-                console.log("Done.");
-            }) */
-});
