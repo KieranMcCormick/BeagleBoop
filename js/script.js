cfdd0065a64fffda6ca303edbd8f9c9c069e811a
Kiearn McCormick <octavian13@shaw.ca>
2017-11-05 13:25:46 -0800

    Board selection started

--- js/script.js
+++ js/script.js
@@ -1,6 +1,33 @@
 var DATAURL = "https://beagle-boop.firebaseio.com/boards/938172/data.json"
 var BOARDURL = "https://beagle-boop.firebaseio.com/boards.json"
 
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+function selectBoard(data) {
+    console.log("test: " + data);
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 $(document).ready(function () {
 
 
@@ -13,14 +40,22 @@ $(document).ready(function () {
             var js = 1
             for (i in data) {
 
-
                 console.log(data[i])
-
+                var item = $('#hiddenLink').clone();
+                item.attr("id", "boardItem")
+                item.attr("onclick", 'selectBoard("' + i + '")');
+                item.text(capitalizeFirstLetter(i));
+                $('#myDropdown').append(item);
 
             }
 
             console.log("Done.");
         })
+
+    $('#btn').click(function () {
+        var id = $(this).attr('id');
+        alert(id);
+    });
 
     /*     $.get({
             url: DATAURL
